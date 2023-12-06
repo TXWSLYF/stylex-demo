b24cdc9d283bfa6d960fad263be981025d24630c
alex <19960623lyf@gmail.com>
2023-12-06 22:43:38 +0900

    init

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,33 @@
-import '../styles/index.scss';
+import * as stylex from "@stylexjs/stylex";
+import { createRoot } from "react-dom/client";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 
-console.log('webpack starterkit');
+console.log(stylex);
+
+const styles = stylex.create({
+  base: {
+    fontSize: 16,
+    lineHeight: 1.5,
+    color: "rgb(60,60,60)",
+  },
+  highlighted: {
+    color: "red",
+  },
+});
+
+console.log(styles);
+
+function MyComponent() {
+  return (
+    <div {...stylex.props(styles.base, styles.highlighted)}>Hello World</div>
+  );
+}
+
+console.log(MyComponent);
+
+// Render your React component instead
+const root = createRoot(document.getElementById("app"));
+root.render(<MyComponent />);
+
+console.log("webpack starterkit");
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,24 +1,30 @@
-const Webpack = require('webpack');
-const { merge } = require('webpack-merge');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const common = require('./webpack.common.js');
+const Webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const common = require("./webpack.common.js");
+const StylexPlugin = require("@stylexjs/webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  stats: 'errors-only',
+  mode: "production",
+  devtool: "source-map",
+  stats: "errors-only",
   bail: true,
   output: {
-    filename: 'js/[name].[chunkhash:8].js',
-    chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
+    filename: "js/[name].[chunkhash:8].js",
+    chunkFilename: "js/[name].[chunkhash:8].chunk.js",
   },
   plugins: [
+    new StylexPlugin({
+      filename: "styles.css",
+      // get webpack mode and set value for dev
+      dev: false,
+    }),
     new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
+      "process.env.NODE_ENV": JSON.stringify("production"),
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[chunkhash:8].css',
-      chunkFilename: 'css/[name].[chunkhash:8].chunk.js',
+      filename: "css/[name].[chunkhash:8].css",
+      chunkFilename: "css/[name].[chunkhash:8].chunk.js",
     }),
   ],
   module: {
@@ -26,11 +32,16 @@ module.exports = merge(common, {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: "babel-loader",
       },
       {
         test: /\.s?css/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
     ],
   },
